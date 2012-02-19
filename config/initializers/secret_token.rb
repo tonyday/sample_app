646e36995e96b50c886d787aec60e9bfae2bc46f
Tony Day <tony@lazylabs.com>
2012-02-19 22:15:29 +1300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '29663b63119f13145b7c48133c79794e16885bcf9d8283a8df9f71a03c9e25120fde863eeb69579c4d3618778a18c4f3bf07ca59f0b95a178083f9b8ed8ca7bf'
